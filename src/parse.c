5763fd31524273562f16b0f1e02cd8d5e2e79bed
milo craun <miloc@vt.edu>
2025-02-07 23:13:26 -0500

    Working on adding string parsing function. Currently does not fully work.

--- src/parse.c
+++ src/parse.c
@@ -10,7 +10,8 @@ char variable_name[256] = {'\0'};
 
 
 enum Symbol sym;
-char * input = "(X AND (NOT Y))";
+char  input[] = "(X AND Y)";
+char * cursor = input;
 char next_token[256] = {'\0'};
 
 int count = 0;
@@ -18,9 +19,56 @@ enum Symbol tokens[] = {lparen, not, variable, and, lparen, variable, or, variab
 
 void nextsym()
 {
-    sym = tokens[count];
-    printf("Sym is %s\n", sym_name[sym]);
-    count++;
+    printf("Input is %s\n", cursor);
+    // Make sure the cursor is not NULL/at the end of the string
+    if (*cursor == '\0') {
+        printf("Error in parsing\n");
+    }
+
+    if (*cursor == '(') {
+        sym = lparen;
+        cursor++;
+    }
+    else if (*cursor == ')') {
+        sym = rparen;
+        cursor++;
+    }
+    else if (*cursor == 'A') {
+        sym = and;
+        cursor += 4;
+    }
+    else if (*cursor == 'O') {
+        sym = or;
+        cursor += 3;
+    }
+    else if (*cursor == 'N') {
+        sym = not;
+        cursor += 4;
+    }
+    else {
+        sym = variable;
+        int idx = 0;
+        while (*cursor != ' ' && *cursor != ')') {
+            variable_name[idx] = *cursor;
+            cursor++;
+            idx++;
+        }
+        if (*cursor != ')') {
+            cursor++;
+        }
+        variable_name[idx] = '\0';
+        //printf("Variable name %s\n", variable_name);
+    }
+
+    printf("Symbol is %s\n", sym_name[sym]);
+    if (sym == variable) {
+        printf("Variable name %s\n", variable_name);
+    }
+    
+
+    //sym = tokens[count];
+    //printf("Sym is %s\n", sym_name[sym]);
+    //count++;
     /* Get the next token and put into next_token */
 }
 
